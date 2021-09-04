2c3b22a3c0d43cabc5d87638652b18807b634c03
FredrikEglese <fredrik.eglese@gmail.com>
2021-09-04 20:06:28 +0100

    Dynamic routing for new bootcamps and carrer data

--- pages/bootcamp/[slug].tsx
+++ pages/bootcamp/[slug].tsx
@@ -7,56 +7,97 @@ import { MenuBar } from "../../components/MenuBar";
 import Layout from "../../components/Layout";
 
 // Services
-import { BootcampDetails, getAllBootcamps, getBootcampBySlug } from "../../services/BootCampService";
+import {
+	BootcampDetails,
+	getAllBootcamps,
+	getBootcampBySlug,
+} from "../../services/BootCampService";
+import { useRouter } from "next/dist/client/router";
 
 interface BootcampProps {
 	bootcampData: BootcampDetails;
 }
 
 export async function getStaticPaths() {
-	const allBootcamps : BootcampDetails[] = await getAllBootcamps();
+	const allBootcamps: BootcampDetails[] = await getAllBootcamps();
 
-  const paths = allBootcamps.map(bootcamp => {
-    return {
+	const paths = allBootcamps.map((bootcamp) => {
+		return {
 			params: {
 				slug: bootcamp.slug,
 			},
 		};
-  });
+	});
 	return {
 		paths,
-		fallback: false,
+		fallback: true,
 	};
 }
 
 export async function getStaticProps({
 	params,
 }): Promise<GetStaticPropsResult<BootcampProps>> {
-	const bootcampData : BootcampDetails = (await getBootcampBySlug(params.slug))[0];
+	const bootcampData: BootcampDetails = (
+		await getBootcampBySlug(params.slug)
+	)[0];
 	return {
 		props: {
 			bootcampData,
 		},
+		revalidate: 5,
 	};
 }
 
 export default function Bootcamp({ bootcampData }: BootcampProps) {
-	const AvailableCarrers = bootcampData.careers.map((career) => {
+	const router = useRouter();
+
+	if (router.isFallback) {
 		return (
-      <li>{career}</li>
+			<Layout>
+				<Head>
+					<title>Loading</title>
+				</Head>
+				<MenuBar />
+				<p>Loading...</p>
+			</Layout>
 		);
-	});
-  
-  return (
+	}
+
+	if (bootcampData == null) {
+		return (
+			<Layout>
+				<Head>
+					<title>No Bootcamp</title>
+				</Head>
+				<MenuBar />
+				<p>No Bootcamp found!</p>
+			</Layout>
+		);
+	}
+
+	const AvailableCarrers = bootcampData.careers
+		? bootcampData.careers.map((career) => {
+				return <li>{career}</li>;
+		  })
+		: [];
+
+	const AvailableCarrersList =
+		AvailableCarrers.length > 0 ? (
+			<ul>{AvailableCarrers}</ul>
+		) : (
+			<p>None at this time</p>
+		);
+
+	return (
 		<Layout>
 			<Head>
 				<title>{bootcampData.name}</title>
 			</Head>
 			<MenuBar />
-      <h1>{bootcampData.name}</h1>
-      <p>{bootcampData.description}</p>
-      <h2>Carrers available:</h2>
-      <ul>{AvailableCarrers}</ul>
+			<h1>{bootcampData.name}</h1>
+			<p>{bootcampData.description}</p>
+			<h2>Carrers available:</h2>
+			{AvailableCarrersList}
 		</Layout>
 	);
 }
